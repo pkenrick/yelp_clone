6cce50c7b5818a844230cecb52322d9eeb7f6810
paulkenrick <pkenrick@gmail.com>
2016-07-13 09:58:35 +0100

    Setup devise and created User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Defines default_url_options for appropriate development environment
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
