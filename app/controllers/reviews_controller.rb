9e4f3ca27eabe2c92f589ec359af686ee6fb26cd
awye765 <awye765@googlemail.com>
2016-07-14 13:47:03 +0100

    Extended ActiveRecord association for restaurant and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,21 +6,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    p "the params are: #{params}"
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.build_review(review_params, current_user)
-    
+    @review = @restaurant.reviews.build_with_user(review_params, current_user)
+
     if @review.save
-      p 'saved ok'
       redirect_to restaurants_path
     else
-      p 'did not save'
       if @review.errors[:user]
-        p @review.errors[:user]
-        p 'there is an error in ":user" key'
         redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
       else
-        p 'there was another error'
         render :new
       end
     end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,15 +2,19 @@ class Restaurant < ActiveRecord::Base
 
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  has_many :reviews, dependent: :destroy
-  
+  has_many :reviews, dependent: :destroy do
+    def build_with_user(attributes = {}, user)
+      attributes[:user] ||= user
+      build(attributes)
+    end
+  end
+
   belongs_to :user
 
-  def build_review(review_params = {}, current_user)
-    review = reviews.build(review_params)
-    review.user = current_user
-
-    review
-  end
+  # def build_review(review_params = {}, current_user)
+  #   review = reviews.build(review_params)
+  #   review.user = current_user
+  #   review
+  # end
 
 end
