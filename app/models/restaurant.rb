42e19e3b86bdac4d8a79d6f15c0c876a5fb695ab
paulkenrick <pkenrick@gmail.com>
2016-07-16 10:22:12 +0100

    Trying out image functionality

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,6 +10,9 @@ class Restaurant < ActiveRecord::Base
 
   belongs_to :user
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def belongs_to?(current_user)
     user == current_user
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,15 @@ Rails.application.configure do
   # Defines default_url_options for appropriate development environment
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # config.paperclip_defaults = {
+  #   :storage => :s3,
+  #   :s3_credentials => {
+  #     :bucket => ENV['S3_BUCKET_NAME'],
+  #     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  #   }
+  # }
+
 end
+
+
